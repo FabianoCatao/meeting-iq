57da8aceec0a2a895c70603a8eb29373069f8c0f
arthur miranda <miranda_chaves@hotmail.com>
2022-06-07 17:05:47 -0300

    building form

--- app/controllers/meetings_controller.rb
+++ app/controllers/meetings_controller.rb
@@ -1,2 +1,21 @@
 class MeetingsController < ApplicationController
+  def new
+    @meeting = Meeting.new
+  end
+
+  def create
+    @meeting = Meeting.new(meeting_params)
+    @meeting.user = current_user
+    if @meeting.save
+      redirect_to meeting_path(@meeting), notice: "Meeting successfully scheduled"
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def meeting_params
+    params.require(:meeting).permit(:title, :goal, :start_date, :location, :description)
+  end
 end
