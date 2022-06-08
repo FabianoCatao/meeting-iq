09de59d9df8c9970057930447fcdfdf2bb6c7221
Guilherme Petkovic <gjrpetkovic@gmail.com>
2022-06-08 12:57:18 -0300

    changed syntax to use %i for array of symbolds for new and create actions in meetings

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,5 @@ Rails.application.routes.draw do
   root to: 'pages#home'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   resources :meetings, only: [:index]
-  resources :meetings, only: [ :new, :create ]
+  resources :meetings, only: %i[new create]
 end
