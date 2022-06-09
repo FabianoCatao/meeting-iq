13f0c14ba7e1418739eda18de7798fb76a6ae910
Guilherme Petkovic <gjrpetkovic@gmail.com>
2022-06-09 14:33:18 -0300

    permitted params for username in application_controller and added validation for username in User model

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,5 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   has_many :meetings, dependent: :destroy
+  validates :username, uniqueness: true, presence: true
 end
