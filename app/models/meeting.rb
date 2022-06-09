86189d468971dcc040f6b60bd9f25eea3ec58c58
Guilherme Petkovic <gjrpetkovic@gmail.com>
2022-06-09 15:09:52 -0300

    added valdiations to Meeting model

--- app/models/meeting.rb
+++ app/models/meeting.rb
@@ -1,3 +1,7 @@
 class Meeting < ApplicationRecord
   belongs_to :user
+
+  validates :expected_start_date, presence: true, uniqueness: { scope: :user_id, message: "already booked for a meeting" }
+  validates :expected_end_date, presence: true
+  validates :participants_number, presence: true
 end
