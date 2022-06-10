b34b3406818df4f919cb1bd45052e6f33d917d58
Guilherme Petkovic <gjrpetkovic@gmail.com>
2022-06-10 13:20:56 -0300

    created delete icon in upcomig and made functional with route, controller action. stylized delete icon and position

--- app/controllers/meetings_controller.rb
+++ app/controllers/meetings_controller.rb
@@ -27,6 +27,12 @@ class MeetingsController < ApplicationController
     end
   end
 
+  def destroy
+    @meeting = Meeting.find(params[:id])
+    @meeting.destroy
+    redirect_to upcoming_meetings_path
+  end
+
   private
 
   def meeting_params
--- config/routes.rb
+++ config/routes.rb
@@ -7,5 +7,5 @@ Rails.application.routes.draw do
       get :upcoming
     end
   end
-  resources :meetings, only: %i[show new create]
+  resources :meetings, only: %i[show new create destroy]
 end
