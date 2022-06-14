cb14728e5372d831b9f11e6d0991a43c799ba14b
mirandanc <miranda_chaves@hotmail.com>
2022-06-14 19:27:56 -0300

    Update application.js

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -15,6 +15,6 @@ ActiveStorage.start()
 import "controllers"
 import "bootstrap"
 
-
+// exemple how you import JS code to run on rails
 // import { meetingTimer } from "./meeting_timer"
 // meetingTimer()
